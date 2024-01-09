4639f89a457c6981892cb2c22db1b7eb81818bf5
Pablo Belusso <121649838+Pablo-Belusso@users.noreply.github.com>
2024-01-09 10:58:30 -0300

    Commit 29

---
+++ MODULO 01/MATRIZES/MATRIZES/MATRIZES/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MATRIZES
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] nome = new string[10]; 
+
+            Console.WriteLine(nome);
+            Console.ReadKey();
+        }
+        
+    }
+}
