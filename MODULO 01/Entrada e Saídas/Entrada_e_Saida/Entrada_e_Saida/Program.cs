914dd0f96e6f6650943d77926080ddad42f48661
Pablo Belusso <121649838+Pablo-Belusso@users.noreply.github.com>
2024-01-15 04:10:14 -0300

    Commit 37

--- MODULO 01/Entrada e Saídas/Entrada_e_Saida/Entrada_e_Saida/Program.cs
+++ MODULO 01/Entrada e Saídas/Entrada_e_Saida/Entrada_e_Saida/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Entrada_e_Saida
 {
@@ -10,6 +11,7 @@ namespace Entrada_e_Saida
     {
         static void Main(string[] args)
         {
+            TextWriter texto = new StreamWriter("Arquivo.txt");
         }
     }
 }
