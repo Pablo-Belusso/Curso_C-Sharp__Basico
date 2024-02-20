f4e6b0ff7e15b906150c7f186325ca5a9582a654
Pablo Belusso <121649838+Pablo-Belusso@users.noreply.github.com>
2024-02-20 15:33:09 -0300

    Commit 102

--- Aula de MVC - Web/WebCrudMVCSQL/WebCrudMVCSQL/Models/Produto.cs
+++ Aula de MVC - Web/WebCrudMVCSQL/WebCrudMVCSQL/Models/Produto.cs
@@ -1,7 +1,13 @@
-﻿namespace WebCrudMVCSQL.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebCrudMVCSQL.Models
 {
+    [Table("Produto")]
     public class Produto
     {
+        [Column("Id")]
+        [Display(Name = "Código")]
         public int Id { get; set; }
     }
 }
