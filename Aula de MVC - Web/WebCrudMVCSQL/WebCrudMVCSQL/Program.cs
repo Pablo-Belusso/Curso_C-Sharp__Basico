89d9945ec42203b8f611fb6fa6fb39c93e1e70fa
Pablo Belusso <121649838+Pablo-Belusso@users.noreply.github.com>
2024-02-22 15:51:02 -0300

    Commit 108

--- Aula de MVC - Web/WebCrudMVCSQL/WebCrudMVCSQL/Program.cs
+++ Aula de MVC - Web/WebCrudMVCSQL/WebCrudMVCSQL/Program.cs
@@ -1,7 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using WebCrudMVCSQL.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<Contexto>
+    (options => options.UseSqlServer(""));
 
 var app = builder.Build();
 
