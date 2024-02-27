e2a0c4c815402832c6ac1b827616aac11aa137bc
Pablo Belusso <121649838+Pablo-Belusso@users.noreply.github.com>
2024-02-27 11:24:45 -0300

    Commit 111

--- Aula de MVC - Web/WebCrudMVCSQL/WebCrudMVCSQL/Program.cs
+++ Aula de MVC - Web/WebCrudMVCSQL/WebCrudMVCSQL/Program.cs
@@ -7,15 +7,16 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 
 builder.Services.AddDbContext<Contexto>
+    // Conexão com o Banco de Dados
     (options => options.UseSqlServer("Data Source=DESKTOP-EUHGO67\\pablo;Initial Catalog=CRUD_MVC_SQL_CANAL_DEV;Integrated Security=False;User ID=pablo;Password=123;Connect Timeout=15;Encrypt=False;TrustServerCertificate=False"));
-
+     
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+   
     app.UseHsts();
 }
 
