2ee33982d79f5b5f1c7b06a8ccc3ed1cf8a53f06
Pablo Belusso <121649838+Pablo-Belusso@users.noreply.github.com>
2024-03-07 12:41:52 -0300

    Commit 131

--- MODULO 02 - POO/pooMensagem/pooMensagem/Program.cs
+++ MODULO 02 - POO/pooMensagem/pooMensagem/Program.cs
@@ -12,11 +12,11 @@ namespace pooMensagem
         {
             Mensagem msg1, msg2;
             msg1 = new Mensagem();
-            msg1.TextoMensagem = "E aeeeee!";            
+            msg1.setTextoMensagem("Hellouuuu!");            
             msg1.ExibirMensagem();
 
             msg2 = new Mensagem();
-            msg2.TextoMensagem = "Cabulosoooo.";
+            //msg2.TextoMensagem = "Cabulosoooo.";
             msg2.ExibirMensagem();
             Console.ReadKey();
         }
