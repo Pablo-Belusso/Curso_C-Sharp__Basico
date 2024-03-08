a65ec1b5f0b7de5721f3574b92390e5e8fe1926c
Pablo Belusso <121649838+Pablo-Belusso@users.noreply.github.com>
2024-03-08 13:32:59 -0300

    Commit 133

--- MODULO 02 - POO/pooMensagem/pooMensagem/Mensagem.cs
+++ MODULO 02 - POO/pooMensagem/pooMensagem/Mensagem.cs
@@ -8,22 +8,22 @@ namespace pooMensagem
 {
     public class Mensagem
     {
-        private String TextoMensagem;
+        //private String TextoMensagem;
 
-        public void ExibirMensagem ()
-        {
-            Console.WriteLine(this.TextoMensagem);
-        }
+        //public void ExibirMensagem ()
+        //{
+        //    Console.WriteLine(this.TextoMensagem);
+        //}
 
-        public String getTextoMensagem()
-        {
-            return this.TextoMensagem; // se quiser não usar o "this" tbm vai funcionar
-        }
+        //public String getTextoMensagem()
+        //{
+        //    return this.TextoMensagem; // se quiser não usar o "this" tbm vai funcionar
+        //}
 
-        public void setTextoMensagem(String valor)
-        {
-            this.TextoMensagem = valor.ToUpper();
-        }
+        //public void setTextoMensagem(String valor)
+        //{
+        //    this.TextoMensagem = valor.ToUpper();
+        //}
     }
 
 }
