328eb9316da2ceb31f4d960a11fb00ac2566bfdc
Pablo Belusso <121649838+Pablo-Belusso@users.noreply.github.com>
2024-04-12 13:43:50 -0300

    Commit 157

---
+++ UDEMY/CURSOS OCTA/C-Sharp - Curso Completo - Basico_Avancado/Curso_C_Sharp_Octa/Curso_C_Sharp_Octa/ClassesEMetodos/Pessoa.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Curso_C_Sharp_Octa.ClassesEMetodos
+{
+    class Pessoa
+    {
+        // Atributos
+        public string Nome;   
+        public int Idade;   
+    }
+}
