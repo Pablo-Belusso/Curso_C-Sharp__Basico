949c2997508278aaff197df0abdde41129303c3d
Pablo Belusso <121649838+Pablo-Belusso@users.noreply.github.com>
2024-04-22 09:28:55 -0300

    Commit 176

---
+++ UDEMY/CURSOS OCTA/C-Sharp - Curso Completo - Basico_Avancado/Curso_C_Sharp_Octa/Curso_C_Sharp_Octa/ClassesEMetodos/Construtores.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Curso_C_Sharp_Octa.ClassesEMetodos
+{
+    class Construtores
+    {
+        public static void Executar()
+        {
+
+        }
+    }
+}
