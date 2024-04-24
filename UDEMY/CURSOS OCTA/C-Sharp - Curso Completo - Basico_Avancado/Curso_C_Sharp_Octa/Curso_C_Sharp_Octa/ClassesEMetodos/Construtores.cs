819bec655b7ab3cf2e77dd0efe7235c72b4af942
Pablo Belusso <121649838+Pablo-Belusso@users.noreply.github.com>
2024-04-24 15:58:36 -0300

    Commit 179

--- UDEMY/CURSOS OCTA/C-Sharp - Curso Completo - Basico_Avancado/Curso_C_Sharp_Octa/Curso_C_Sharp_Octa/ClassesEMetodos/Construtores.cs
+++ UDEMY/CURSOS OCTA/C-Sharp - Curso Completo - Basico_Avancado/Curso_C_Sharp_Octa/Curso_C_Sharp_Octa/ClassesEMetodos/Construtores.cs
@@ -6,6 +6,13 @@ using System.Threading.Tasks;
 
 namespace Curso_C_Sharp_Octa.ClassesEMetodos
 {
+
+    class Carro
+    {
+        public string Modelo;
+        public string Fabricante;
+        public int Ano;
+    }
     class Construtores
     {
         public static void Executar()
