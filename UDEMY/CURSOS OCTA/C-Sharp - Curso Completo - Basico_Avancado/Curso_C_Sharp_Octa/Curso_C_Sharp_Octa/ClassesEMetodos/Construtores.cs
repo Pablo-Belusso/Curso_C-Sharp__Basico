ae688e6031a758473c98397060fe0bef63a26445
Pablo Belusso <121649838+Pablo-Belusso@users.noreply.github.com>
2024-05-06 17:08:44 -0300

    Commit 189

--- UDEMY/CURSOS OCTA/C-Sharp - Curso Completo - Basico_Avancado/Curso_C_Sharp_Octa/Curso_C_Sharp_Octa/ClassesEMetodos/Construtores.cs
+++ UDEMY/CURSOS OCTA/C-Sharp - Curso Completo - Basico_Avancado/Curso_C_Sharp_Octa/Curso_C_Sharp_Octa/ClassesEMetodos/Construtores.cs
@@ -20,12 +20,16 @@ namespace Curso_C_Sharp_Octa.ClassesEMetodos
             Fabricante = fabricante;
             Ano = ano;
         }
+
+        public Carro() {
+        
+        }
     }
     class Construtores
     {
         public static void Executar()
         {
-            var carro1 = new Carro("I3","Toyota",2012);
+            var carro1 = new Carro();
             carro1.Fabricante = "BMW";
             carro1.Modelo = "325i";
             carro1.Ano = 2017;
@@ -33,6 +37,13 @@ namespace Curso_C_Sharp_Octa.ClassesEMetodos
 
             var carro2 = new Carro("Ka", "Ford", 2018);
             Console.WriteLine($"Carro: {carro2.Fabricante} {carro2.Modelo} {carro2.Ano}");
+
+            var carro3 = new Carro()
+            {
+                Fabricante = "Fiat",
+                Modelo = "Uno",
+                Ano = 2018
+            };
 
             Console.ReadLine();
         }
