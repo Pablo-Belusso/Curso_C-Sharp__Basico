ffeb2e924c7da7f5f0ceb45596b48e2c957d4d07
Pablo Belusso <121649838+Pablo-Belusso@users.noreply.github.com>
2024-05-07 14:51:43 -0300

    Commit 194

--- UDEMY/CURSOS OCTA/C-Sharp - Curso Completo - Basico_Avancado/Curso_C_Sharp_Octa/Curso_C_Sharp_Octa/ClassesEMetodos/MetodoComRetorno.cs
+++ UDEMY/CURSOS OCTA/C-Sharp - Curso Completo - Basico_Avancado/Curso_C_Sharp_Octa/Curso_C_Sharp_Octa/ClassesEMetodos/MetodoComRetorno.cs
@@ -6,6 +6,10 @@ using System.Threading.Tasks;
 
 namespace Curso_C_Sharp_Octa.ClassesEMetodos
 {
+    class CalculadoraComum
+    {
+
+    }
     class MetodoComRetorno
     {
         public static void Executar() {
